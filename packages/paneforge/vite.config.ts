f854834694fdcb9ba4bae33159405c955ab60ae9
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2024-07-29 22:48:15 -0400

    oops

--- packages/paneforge/vite.config.ts
+++ packages/paneforge/vite.config.ts
@@ -6,7 +6,4 @@ export default defineConfig({
 	test: {
 		include: ["src/**/*.{test,spec}.{js,ts}"],
 	},
-	optimizeDeps: {
-		exclude: ["paneforge"],
-	},
 });
--- sites/docs/vite.config.ts
+++ sites/docs/vite.config.ts
@@ -18,4 +18,7 @@ export default defineConfig({
 			},
 		],
 	},
+	optimizeDeps: {
+		exclude: ["paneforge"],
+	},
 });
