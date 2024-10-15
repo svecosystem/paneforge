0bbc38bbcafeccae178f442a50414cba4e865eec
Hunter Johnston <johnstonhuntera@gmail.com>
2024-10-14 21:09:20 -0400

    fix: use-id

--- packages/paneforge/src/lib/internal/utils/useId.ts
+++ packages/paneforge/src/lib/internal/utils/useId.ts
@@ -4,6 +4,6 @@ let count = 0;
  * Generates a unique ID based on a global counter.
  */
 export function useId(prefix = "paneforge") {
-	count = count++;
+	count++;
 	return `${prefix}-${count}`;
 }
