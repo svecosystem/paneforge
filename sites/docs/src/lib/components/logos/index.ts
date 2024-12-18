b8f46f5dfaa8ddd2a2c495d858b10322028c7e34
Hunter Johnston <johnstonhuntera@gmail.com>
2024-12-18 10:35:36 -0500

    formatting

--- sites/docs/src/lib/components/logos/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as SvecosystemDark } from "./svecosystem-dark.svelte";
-export { default as SvecosystemLight } from "./svecosystem-light.svelte";
-export { default as PaneforgeDark } from "./paneforge-dark.svelte";
-export { default as PaneforgeLight } from "./paneforge-light.svelte";
